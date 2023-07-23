c60c7782d3a0ea34ed3327bea84a0c7822c1bf2c
PatrickOhanlon1 <135080049+PatrickOhanlon1@users.noreply.github.com>
2023-07-23 08:26:23 +0000

    HW #7 fixed Cmake list linking wrong folder

--- src/homework/07_arrays/main.cpp
+++ src/homework/07_arrays/main.cpp
@@ -1,4 +1,26 @@
+#include "arrays.h"
+
 int main()
 {
+    const int SIZE = 5;
+    int list[SIZE];
+
+    ArrayList array_list(list, SIZE);
+
+    array_list.Add(5);
+    array_list.Add(7);
+    array_list.Add(10);
+    array_list.Add(15);
+    array_list.Add(20);
+
+    array_list.Delete(5);
+
+     int test_arr[] = {7, 10, 15, 20};
+
+     for (int i = 0; i < array_list.Size(); i++) 
+     {
+        std::cout << array_list.Get_Value_At_Index(i);
+     }
+
     return 0;
 }
